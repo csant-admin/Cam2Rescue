6967dbcd3d29512fbde090c05584002fb3dccb04
csant-admin <santandercharlie.mail@gmail.com>
2023-01-28 13:42:32 +0800

    Update index.php file

--- Cam2rescue-mini/index.php
+++ Cam2rescue-mini/index.php
@@ -4,6 +4,24 @@
         <title></title>
     </head>
     <body>
+        <header>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Sign-up</li>
+                    <li>Login</li>
+                    <li>my account</li>
+                </ul>
+            </nav>
+        </header>
         <h1>Cam2Rescue</h1>
+        <section>
+            <div class="">
+                <h4>Cats and Dogs</h4>
+                <p class=""><span>Adopt</span>Any Pet You Like!</p>
+            </div>
+        </section>
     </body>
 </html>
