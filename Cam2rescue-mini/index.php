fa20bd9a3ec96f12b1839a74ae60f80333b2d288
csant-admin <santandercharlie.mail@gmail.com>
2023-01-28 21:42:03 +0800

    update index file

--- Cam2rescue-mini/index.php
+++ Cam2rescue-mini/index.php
@@ -1,42 +1,32 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title></title>
-        <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
-        <script src="assets/js/index.js"></script>
+        <title>An Online Platform for Pet Rescue and Shelters</title>
+        <link rel="stylesheet" href="assets/css/index.css">
     </head>
     <body>
         <header>
             <nav>
                 <ul>
-                    <li><button class="home-btn">Home</button></li>
-                    <li><button class="about-btn">About</button></li>
-                    <li><button class="contact-btn">Contact</button></li>
-                    <li><button class="signup-btn">Sign-up</button></li>
-                    <li><button class="login-btn">Login</button></li>
-                    <li><button class="account-btn">my account</button></li>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Sign-up</li>
+                    <li>Login</li>
+                    <li>my account</li>
                 </ul>
             </nav>
         </header>
         <h1>Cam2Rescue</h1>
         <section>
-            <div id="home">
-                <div class="">
+            <div class="row">
+                <div class="column">
                     <h4>Cats and Dogs</h4>
-                    <p class=""><span>Adopt</span>Any Pet You Like!</p>
+                    <p class=""><span>Adopt</span> Any Pet You Like!</p>
                 </div>
-            </div>
-
-            <div id="about">
-                <p>This is about page</p>
-            </div>
-
-            <div id="contact">
-                <p>This is contact page</p>
-            </div>
-
-            <div id="signup">
-                <p>This is sign up page</p>
+                <div class="column">
+                    <img src="assets/img/catt.jpg" alt="cat and dog">
+                </div>
             </div>
         </section>
     </body>
