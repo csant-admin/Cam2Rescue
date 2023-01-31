b0a99c262234e7158d194d2ad4d8c48c4e199856
csant-admin <santandercharlie.mail@gmail.com>
2023-01-31 22:48:04 +0800

    change some jquery

--- Cam2rescue-mini/database.php
+++ Cam2rescue-mini/database.php
@@ -5,7 +5,7 @@
           $username = "root";
           $password = "";
           try{
-           $conn = new PDO("mysql:host=$server; dbname=cam2rescue_mini", $username, $password);
+            $conn = new PDO("mysql:host=$server; dbname=cam2rescue_mini", $username, $password);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
             return $conn;
